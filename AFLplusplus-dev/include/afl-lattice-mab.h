6cb89dd87f507b91536a933b2f6074d7c5106757
apulis-dev <123@google.com>
2025-11-26 08:18:03 +0000

    add stable1 git-commit.sh

--- AFLplusplus-dev/include/afl-lattice-mab.h
+++ AFLplusplus-dev/include/afl-lattice-mab.h
@@ -30,16 +30,16 @@ typedef struct afl_state afl_state_t;
 /* Note: MUT_MAX is defined in afl-mutations.h, but we avoid including it here
    to prevent circular dependencies. The value 37 matches MUT_MAX. */
 #define LATTICE_DIMENSION 37  /* Number of mutation types (MUT_MAX) */
-#define MAB_ALPHA 0.01        /* UCB exploration parameter (increased for better exploration) */
-#define MAB_EPSILON 0.05      /* Epsilon-greedy parameter (increased for better exploration) */
+#define MAB_ALPHA 0.004        /* UCB exploration parameter (increased for better exploration) */
+#define MAB_EPSILON 0.02      /* Epsilon-greedy parameter (increased for better exploration) */
 #define LATTICE_NEIGHBOR_RADIUS 3  /* Radius for nearest neighbor search */
 #define MAB_WINDOW_SIZE 1000  /* Window size for reward tracking */
-#define EFFICIENCY_PENALTY_FACTOR 0.3  /* Reduced penalty to avoid premature abandonment */
-#define NEIGHBOR_EXPLORE_PROB 5  /* Increased probability of exploring neighbors */
-#define EFFICIENCY_THRESHOLD 50  /* Increased threshold to allow more exploration */
-#define MIN_EFFICIENCY_RATIO 0.00005  /* Reduced minimum ratio to be less restrictive */
-#define EFFICIENCY_REWARD_WEIGHT 25.0  /* Balanced weight for efficiency reward */
-#define COVERAGE_REWARD_WEIGHT 10.0  /* Increased weight for direct coverage gain */
+#define EFFICIENCY_PENALTY_FACTOR 0.5  /* Reduced penalty to avoid premature abandonment */
+#define NEIGHBOR_EXPLORE_PROB 2  /* Increased probability of exploring neighbors */
+#define EFFICIENCY_THRESHOLD 30  /* Increased threshold to allow more exploration */
+#define MIN_EFFICIENCY_RATIO 0.0001  /* Reduced minimum ratio to be less restrictive */
+#define EFFICIENCY_REWARD_WEIGHT 30.0  /* Balanced weight for efficiency reward */
+#define COVERAGE_REWARD_WEIGHT 15.0  /* Increased weight for direct coverage gain */
 
 /* Mutation Vector Structure */
 typedef struct {
